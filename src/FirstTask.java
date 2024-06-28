ab32a9b76153b22ae43e6d97cafdf7c2ee78697e
alex_ <alex_7377@hotmail.com>
2024-06-28 02:21:30 +0200

    Initial commit

---
+++ src/FirstTask.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+
+public class FirstTask {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        List<Integer> integers = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            integers.add(random.nextInt(100));
+        }
+        System.out.println("Исходный массив :  " + integers);
+
+        Optional<Integer> result = integers.stream()
+                .map(x -> x + 15)           //    К каждому числу добавить 15
+                .filter(x -> x > 50)         //    Оставить числа, большие 50
+                .reduce(Integer::sum);    //    Найти сумму чисел
+        System.out.println("Сумма чисел равна :  "+result.get());
+    }
+}
---
+++ src/SecondTask.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class SecondTask {
+
+    public static void main(String[] args) {
+
+        List<String> strings = new ArrayList<>();
+        strings.add("Белая берёза");
+        strings.add("Под моим окном");
+        strings.add("Принакрылась снегом,");
+        strings.add("Точно серебром.");
+        strings.add("На пушистых ветках");
+        strings.add("Снежною каймой");
+        strings.add("Распустились кисти");
+        strings.add("Белой бахромой.");
+        strings.add("И стоит берёза");
+        strings.add("В сонной тишине,");
+        strings.add("И горят снежинки");
+        strings.add("В золотом огне.");
+        strings.add("А заря, лениво");
+        strings.add("Обходя кругом,");
+        strings.add("Обсыпает ветки");
+        strings.add("Новым серебром.");
+
+        List<String> filteredStrings = strings.stream()
+                .filter(str -> str.length() <= 15)              // Отфилтровать строки длиннее 15 символов
+                .toList();
+
+        String longestString = filteredStrings.stream()
+                .max(Comparator.comparingInt(String::length))   // Найти самую длинную строку среди оставшихся
+                .get();
+
+        System.out.println("Отфильтрованные строки :\n" + filteredStrings);
+        System.out.println("\nСамая длинная строка среди отфильтрованных :\n" + longestString);
+
+    }
+}
